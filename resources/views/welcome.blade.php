4ca3c4db47eceb0994189e47c47d3824a80fc8ec
huangqijun <hqjjsw@163.com>
2015-09-23 11:00:34 +0800

    code adjust

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>金蛋篮科技有限公司</title>
 
         
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">金蛋篮科技有限公司</div>
             </div>
         </div>
     </body>
