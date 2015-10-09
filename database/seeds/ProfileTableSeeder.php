8d70b8dda37724a6261aab6b06f7ea580f448298
huangqijun <hqjjsw@163.com>
2015-10-09 19:10:05 +0800

    data seeder modify

--- database/seeds/ProfileTableSeeder.php
+++ database/seeds/ProfileTableSeeder.php
@@ -12,22 +12,44 @@ class ProfileTableSeeder extends Seeder
     public function run()
     {
         DB::table('platforms')->insert([
-		    ['platform_name' => '人人贷']
+		    ['platform_name' => '人人贷'],
+		    ['platform_name' => '点融网']
 		]);
 
 		DB::table('category_profiles')->insert([
-		    ['category_name' => 'U计划-A', 'platform_id' => 1]
+		    ['category_name' => 'U计划-A', 'platform_id' => 1],
+		    ['category_name' => 'U计划-B', 'platform_id' => 1],
+		    ['category_name' => 'U计划-C', 'platform_id' => 1]
 		]);
 
 		DB::table('crawler_profiles')->insert([
-		    ['category_id' => 1, 'field_name' => 'product_name', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#plan-basic-panel>div.ui-box-white-bg>div.ui-box-title>h3&text'],
-		    ['category_id' => 1, 'field_name' => 'rate', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'em.text-xxxl&text'],
-		    ['category_id' => 1, 'field_name' => 'investment_period', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '30'],
-		    ['category_id' => 1, 'field_name' => 'progress', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-progress-value>em&text'],
-		    ['category_id' => 1, 'field_name' => 'start_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value>em&text'],
-		    ['category_id' => 1, 'field_name' => 'available_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#investment-terminal>dl>dd.mt6>em&text'],
-		    ['category_id' => 1, 'field_name' => 'safeguard_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value&text'],
-		    ['category_id' => 1, 'field_name' => 'income_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=642', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '1.收益再投资.\n2.提取至账户']
+			//人人贷
+		    ['category_id' => 1, 'field_name' => 'product_name', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#plan-basic-panel>div.ui-box-white-bg>div.ui-box-title>h3&text'],
+		    ['category_id' => 1, 'field_name' => 'rate', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'em.text-xxxl&text'],
+		    ['category_id' => 1, 'field_name' => 'investment_period', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '90'],
+		    ['category_id' => 1, 'field_name' => 'progress', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-progress-value>em&text'],
+		    ['category_id' => 1, 'field_name' => 'start_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value>em&text'],
+		    ['category_id' => 1, 'field_name' => 'available_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#investment-terminal>dl>dd.mt6>em&text'],
+		    ['category_id' => 1, 'field_name' => 'safeguard_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value&text'],
+		    ['category_id' => 1, 'field_name' => 'income_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=643', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '1.收益再投资.\n2.提取至账户'],
+
+		    ['category_id' => 2, 'field_name' => 'product_name', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#plan-basic-panel>div.ui-box-white-bg>div.ui-box-title>h3&text'],
+		    ['category_id' => 2, 'field_name' => 'rate', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'em.text-xxxl&text'],
+		    ['category_id' => 2, 'field_name' => 'investment_period', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '180'],
+		    ['category_id' => 2, 'field_name' => 'progress', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-progress-value>em&text'],
+		    ['category_id' => 2, 'field_name' => 'start_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value>em&text'],
+		    ['category_id' => 2, 'field_name' => 'available_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#investment-terminal>dl>dd.mt6>em&text'],
+		    ['category_id' => 2, 'field_name' => 'safeguard_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value&text'],
+		    ['category_id' => 2, 'field_name' => 'income_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=645', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '1.收益再投资.\n2.提取至账户'],
+
+		    ['category_id' => 3, 'field_name' => 'product_name', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#plan-basic-panel>div.ui-box-white-bg>div.ui-box-title>h3&text'],
+		    ['category_id' => 3, 'field_name' => 'rate', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'em.text-xxxl&text'],
+		    ['category_id' => 3, 'field_name' => 'investment_period', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '360'],
+		    ['category_id' => 3, 'field_name' => 'progress', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-progress-value>em&text'],
+		    ['category_id' => 3, 'field_name' => 'start_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value>em&text'],
+		    ['category_id' => 3, 'field_name' => 'available_investment_amount', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'div#investment-terminal>dl>dd.mt6>em&text'],
+		    ['category_id' => 3, 'field_name' => 'safeguard_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'DomValue', 'field_value' => 'span.basic-value&text'],
+		    ['category_id' => 3, 'field_name' => 'income_mode', 'field_url' => 'http://www.renrendai.com/financeplan/listPlan!detailPlan.action?financePlanId=647', 'url_type' => 'Normal', 'field_type' => 'Fixed', 'field_value' => '1.收益再投资.\n2.提取至账户'],
 
 		]);
     }
